28a0296fd1015640aa503dd9ceb5b366309b5625
Akansha Sharma <akansha.d.s@gmail.com>
2016-01-10 21:42:59 -0500

    CAN

--- Neighbor.java
+++ Neighbor.java
@@ -1,9 +1,6 @@
 import java.io.Serializable;
 
 public class Neighbor implements Serializable {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	public double x1;
 	public double x2;
